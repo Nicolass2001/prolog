8bcd4906e4a3ad4d2e6ef6ca24d73e5eebfbd04e
Nicolas Pereira <nicolas.pp333@gmail.com>
2025-05-29 19:11:04 -0300

    Practico 8: Ejercicio 4

--- practico/p8.pl
+++ practico/p8.pl
@@ -137,3 +137,102 @@ sumab(X, Y, Z):-
     nonvar(Z),
     between(0, Z, X),
     Y is Z - X.
+
+/* Ejercicio 4 [Fundamental]
+Considere la representación de matrices mediante functores en Prolog. Una matriz
+se representa como un functor m aplicado a una serie de filas, y cada fila es un
+functor f aplicado a una serie de celdas. Por ejemplo, la siguiente matriz de tamaño
+2x3:
+1 2 3
+4 5 6
+se representa como: m(f(1, 2, 3),f(4, 5, 6))
+Implemente los siguientes predicados:
+
+matriz(+F,+C,+V,-M) ← M es una matriz de F filas y C columnas donde cada celda tiene el valor V
+
+celda(+M,?I,?J,?V) ← V es el valor de la celda (I,J) de la matriz M
+
+nuevo_valor(+M,+I, +J,+V) ← Se sustituye el valor de la celda (I,J) de la matriz M por V*
+
+suma(+M,+N,?S) ← S es la suma de las matrices M y N
+
+Notar que nuevo_valor no tiene argumento de salida. Se sugiere investigar el
+predicado extralógico set_arg/3 de SWI-Prolog.
+*/
+
+% matriz(+F,+C,+V,-M) ← M es una matriz de F filas y C columnas donde cada celda tiene el valor V
+generar_lista(0,_,[]):- !.
+generar_lista(N,V,[V|Lista]):-
+    NNew is N - 1,
+    generar_lista(NNew,V,Lista).
+
+generar_lista_filas(0,_,_,[]):- !.
+generar_lista_filas(F,C,V,[Fila|ListaFilas]):-
+    generar_lista(C,V,ListaFila),
+    Fila =.. [f|ListaFila],
+    FNew is F - 1,
+    generar_lista_filas(FNew,C,V,ListaFilas).
+
+matriz(F,C,V,M):-
+    generar_lista_filas(F,C,V,ListaFilas),
+    M =.. [m|ListaFilas].
+
+% celda(+M,?I,?J,?V) ← V es el valor de la celda (I,J) de la matriz M
+celda(M,I,J,V):-
+    M =.. [_|Filas],
+    nth1(I,Filas,Fila),
+    Fila =.. [_|Valores],
+    nth1(J,Valores,V).
+
+% nuevo_valor(+M,+I, +J,+V) ← Se sustituye el valor de la celda (I,J) de la matriz M por V*
+nuevo_valor(M,I,J,V):-
+    M =.. [_|Filas],
+    nth1(I,Filas,Fila),
+    setarg(J,Fila,V).
+
+% suma(+M,+N,?S) ← S es la suma de las matrices M y N
+suma_valores([],[],[]):- !.
+suma_valores([Vm|M],[Vn|N],[Vs|S]):-
+    Vs is Vm + Vn,
+    suma_valores(M,N,S).
+
+
+suma_filas(M,N,S):-
+    M =.. [_|ValoresM],
+    N =.. [_|ValoresN],
+    length(ValoresM,F),
+    length(ValoresN,F),
+    suma_valores(ValoresM,ValoresN,ValoresS),
+    S =.. [f|ValoresS].
+
+suma_filas_aux([],[],[]):- !.
+suma_filas_aux([Fm|M],[Fn|N],[Fs|S]):-
+    suma_filas(Fm,Fn,Fs),
+    suma_filas_aux(M,N,S).
+
+suma_matriz(M,N,S):-
+    M =.. [_|FilasM],
+    N =.. [_|FilasN],
+    length(FilasM,F),
+    length(FilasN,F),
+    suma_filas_aux(FilasM,FilasN,FilasS),
+    S =.. [m|FilasS].
+
+/* Ejercicio 5 [Fundamental]
+Sea el siguiente programa Prolog:
+    padre(juan, ana).
+    padre(juan, jose).
+    padre(juan, pedro).
+    padre(pedro, hector).
+    padre(pedro, gustavo).
+    padre(hector, maria).
+Indique las respuestas que se obtienen para L con los siguientes objetivos:
+    i. findall(X, padre(juan,X), L).
+    ii. findall(X, padre(Y,X), L).
+    iii. findall(X, (padre(juan,X) ; padre(pedro,X)), L).
+    iv. findall(X, (padre(juan,X), padre(X,Y)), L).
+    v. setof(X, padre(juan,X), L).
+    vi. setof(X, padre(Y,X), L).
+    vii. setof(X, (padre(juan,X) ; padre(pedro,X)), L).
+    viii. setof(X, (padre(juan,X), padre(X,Y)), L).
+*/
